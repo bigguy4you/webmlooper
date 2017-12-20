d53ab55a18fcb4e1be778ebf542830b9d0727c19
bigguy4you <34697248+bigguy4you@users.noreply.github.com>
2017-12-20 04:50:19 +0300

    Add files via upload

---
+++ .mp4_&_.webm_looper_1_2.user.js
@@ -0,0 +1,12 @@
+﻿// ==UserScript==
+// @name        .mp4 & .webm looper
+// @namespace   bigguy4you
+// @description loops HTML5 videos
+// @include     *.webm*
+// @include     *.mp4*
+// @run-at      document-start
+// @version     1.2
+// @grant       none
+// ==/UserScript==
+var video = document.querySelector('body > video');
+video.loop = true;
